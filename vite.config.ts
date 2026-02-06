7d338cfbcfbf32133b5b1cecb63900be23b8d4db
belinkedin <belinkedindindigul@gmail.com>
2026-02-06 22:55:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/Shri-Ashtalakshmi-Electricals',
-  define: {
-    'import.meta.env.VITE_GOOGLE_SCRIPT_URL': JSON.stringify(process.env.VITE_GOOGLE_SCRIPT_URL)
-  }
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    // Set base path for GitHub Pages deployment
+    base: process.env.NODE_ENV === 'production' ? '/Shri-Ashtalakshmi-Electricals/' : '/',
+
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'import.meta.env.VITE_GOOGLE_SCRIPT_URL': JSON.stringify(process.env.VITE_GOOGLE_SCRIPT_URL)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      }
+    }
+  };
+});
